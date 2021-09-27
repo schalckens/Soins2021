3cb46feaed83ac014d2244c3ac32fc139035444f
Valentine SCHALCKENS <v.schalckens@gmail.com>
2021-09-28 00:59:26 +0200

    Avancement evolution 5

---
+++ Soins2021/Exceptions/SoinException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soins2021.Exceptions
+{
+    class SoinException : Exception
+    { 
+        private string message;
+        private static string json;
+        private static List<TempException> listEx;
+
+        public SoinException(string message) :base(message) 
+        {
+            
+            TempException log = new TempException("SoinException", message, Environment.UserName.ToString(), Environment.MachineName.ToString());
+
+            json = JsoncConvert.SerializeObject(log);
+            
+        }
+        public string Message1 { get => message; set => message = value; }
+    }
+}
---
+++ Soins2021/Exceptions/TempException.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soins2021.Exceptions
+{
+    internal class TempException
+    {
+        private string application;
+        private string classeException;
+        private DateTime dateException;
+        private string messageException;
+        private string userException;
+        private string userMachine;
+
+        public TempException(string classeException,string messageException, string userException, string userMachine)
+        {
+            this.Application = "Soins2021";
+            this.ClasseException = classeException;
+            this.DateException = DateTime.Now;
+            this.messageException = messageException;
+            this.userException = userException;
+            this.userMachine = userMachine;
+        }
+
+        public string Application { get => application; set => application = value; }
+        public string ClasseException { get => classeException; set => classeException = value; }
+        public DateTime DateException { get => dateException; set => dateException = value; }
+        public string MessageException { get => messageException; set => messageException = value; }
+        public string UserException { get => userException; set => userException = value; }
+        public string UserMachine { get => userMachine; set => userMachine = value; }
+    }
+}
